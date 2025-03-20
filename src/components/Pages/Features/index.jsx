b0b4007daf82426e2efbac7b14999d25b8932c20
Prity25-coder <pritynishad480@gmail.com>
2025-03-20 13:03:49 +0530

    modify

--- src/components/Pages/Features/index.jsx
+++ src/components/Pages/Features/index.jsx
@@ -19,7 +19,7 @@ function Features({ onClose }) {
           1. Visual Learning Enhances Understanding
         </h5>
 
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           Videos provide animated explanations, real-world examples, and
           step-by-step problem-solving that help students grasp complex IB
           concepts more easily than just reading textbooks.
@@ -31,7 +31,7 @@ function Features({ onClose }) {
           2.Engaging & Interactive
         </h5>
 
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           Unlike static text, videos are engaging and dynamic, keeping students
           focused and motivated while studying.
         </p>
@@ -41,10 +41,10 @@ function Features({ onClose }) {
         <h5 className="flex justify-start font-bold">
           3. Learn at Your Own Pace
         </h5>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Pause, rewind, and replay difficult topics anytime.
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Speeds up revision before IB exams
         </p>
       </div>
@@ -53,7 +53,7 @@ function Features({ onClose }) {
         <h5 className=" flex justify-start font-bold">
           4. Access to Expert IB Tutors
         </h5>
-        <p className=" flex justify-start">
+        <p className=" flex justify-start font-light">
           High-quality video content is created by experienced IB educators,
           ensuring students receive accurate and effective explanations.
         </p>
@@ -63,16 +63,16 @@ function Features({ onClose }) {
         <h5 className="flex justify-start font-bold">
           5. Covers All IB Subjects
         </h5>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           Videos provide structured lessons across subjects like:-
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Mathematics: Step-by-step problem-solving
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           -Sciences: 3D animations of biological & chemical processes
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Humanities: In-depth historical & literary analysis
         </p>
       </div>
@@ -81,13 +81,13 @@ function Features({ onClose }) {
         <h5 className="flex justify-start font-bold">
           6. Perfect for IB Exam Preparation
         </h5>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Exam-style question walkthroughs.
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Past paper solutions & strategies
         </p>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           - Time management tips for IB assessments
         </p>
       </div>
@@ -96,28 +96,24 @@ function Features({ onClose }) {
         <h5 className="flex justify-start font-bold">
           7. Study Anytime, Anywhere
         </h5>
-        <p className="flex justify-start">
+        <p className="flex justify-start font-light">
           -Available on mobile, tablet, and desktopAvailable on mobile, tablet,
           and desktop
         </p>
-        <p className="flex justify-start">- Learn on-the-go with 24/7 access</p>
+        <p className="flex justify-start font-light">
+          - Learn on-the-go with 24/7 access
+        </p>
       </div>
 
       <div className="ml-8 mt-4 mb-10">
         <h5 className="flex justify-start font-bold">
           Boosts IB Scores & Confidence
         </h5>
-        <p className=" flex justify-start">
+        <p className=" flex justify-start font-light">
           Students using videos for IB preparation often score higher, as they
           understand concepts better and retain information more effectively.
         </p>
       </div>
-
-      {/* <ul>
-        <li>Interoducing Nail IB Videos</li>
-        <li>benefits of videos for IB study</li>
-        <li>How to access video step-by-step process</li>
-      </ul> */}
     </div>
   );
 }
